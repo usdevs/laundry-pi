80fea9f339d0c7652efbf867f420bf7ee669d03e
Mitchell Kwong <mitchellkwong@users.noreply.github.com>
2019-08-30 23:17:23 +0800

    Handle I/O stuff asynchronously (tried my best)

--- firebasePush.py
+++ firebasePush.py
@@ -1,66 +1,94 @@
+import asyncio
 import time
-import board
-import busio
+
 import firebase_admin
-import time
 from firebase_admin import credentials
 from firebase_admin import firestore
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
 
-# Using guide from https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15
+FIREBASE_CERT = './usc-laundry-test-firebase-adminsdk-0sph5-01054b85e5.json'
+FIREBASE_COLLECTION = 'test_collection'
+FIREBASE_DOCUMENT = 'test_document'
+THRESHHOLD = 32000
 
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+class FirebaseManager:
+    def __init__(self, cert, interval=0.5):
+        self.db = self.init_database(cert)
+        self.machines = list()
 
-# Create ADC object using I2C bus
-ads1 = ADS.ADS1115(i2c, address=0x49)
-ads2 = ADS.ADS1115(i2c, address=0x48)
-ads3 = ADS.ADS1115(i2c, address=0x4a)
+    def init_database(self, cert):
+        cred = credentials.Certificate(cert)
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+        doc = db.collection(FIREBASE_COLLECTION).document(FIREBASE_DOCUMENT)
+        return doc
 
-# create single-ended input on channel 0
-#chan = [AnalogIn(ads, ADS.P0), AnalogIn(ads, ADS.P1)]
-#chan = [AnalogIn(ads, ADS.P1), AnalogIn(ads, ADS.P0)]
-#print("{:>5}\t{:>5}".format('raw', 'v'))
+    def add_machine(self, machine):
+        self.machines.append(machine)
 
-cred = credentials.Certificate('./usc-laundry-test-firebase-adminsdk-0sph5-01054b85e5.json')
+    def run(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.main())
 
-firebase_admin.initialize_app(cred)
+    async def push(self, data):
+        self.db.set(data, merge=True)
 
-db = firestore.client()
+    async def main(self):
+        while True:
+            status = list()
+            await asyncio.gather(*(machine.write_status(status) for machine in self.machines))
+            await asyncio.gather(*(self.push(data) for data in status))
+            await asyncio.sleep(self.interval)
 
-doc_ref = db.collection("test_collection").document("test_document")
+class LaundryMachine:
+    def __init__(self, name, sensor):
+        self.name = name
+        self.running = None
 
-generalThresholdValue = 32000
-machine0starttime = 0
-machine0status = "off"
+    async def write_status(self, record):
+        running = await self.sensor.status()
+        if running != self.running:
+            self.running = running
+            record.append({
+                f'{self.name}_running': self.running,
+                f'{self.name}_time': time.time(),
+            })
 
-while True:
-    lightnessValue0 = AnalogIn(ads1, ADS.P0).value
-    #lightnessValue1 = AnalogIn(ads, ADS.P1).value
-    if lightnessValue0 < generalThresholdValue:
-        if machine0status == "off":
-            machine0starttime = time.time()
-        machine0status = "on"
-    else:
-        if machine0status == "on":
-            machine0time = 0
-        machine0status = "off"
-    #if lightnessValue1 > generalThresholdValue:
-    #    machine1status = "on"
-    #else:
-    #    machine1status = "off"
-    print("pin 0 " + str(lightnessValue0))
-    #print("pin 1 " + str(lightnessValue1))
-    if machine0status == "on":
-        machine0TimeRemaining = time.time() - machine0starttime
-    else:
-        machine0TimeRemaining = 0
-    doc_ref.set({
-        "machine0":machine0status,
-        "machine0TimeRemaining":machine0TimeRemaining
-    })
-    time.sleep(0.1)
+    async def update_status(self):
+        """Update self.running and self.start_time"""
+        raise NotImplementedError('Machine should be subclassed')
 
-#LDR resistance decreases with increasing light intensity
-#light on > lower ADC values
+class ADS1x15:
+    def __init__(self, ads):
+        self.ads = ads
+
+    async def status(self):
+        lightness = AnalogIn(self.ads, ADS.P0).value
+        return lightness < THRESHHOLD
+
+if __name__ == '__main__':
+    # Initialize firebase
+    fb = FirebaseManager(CERT_PATH, interval=0.5)
+
+    # Create the I2C bus
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # Create ADC object using I2C bus
+    ads_inputs = [
+        ADS.ADS1115(i2c, address=0x49),
+        ADS.ADS1115(i2c, address=0x48),
+        ADS.ADS1115(i2c, address=0x4a),
+    ]
+
+    # Wrap ADCs in an interface before passing to firebase Object
+    machines = [LaundryMachine(ADS1x15(input)) for input in ads_inputs]
+    for machine in machines:
+        fb.add_machine(machine)
+
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(fb.run())
+
+    except Exception as e:
+        loop.stop()
+        print(e)
+        input()
