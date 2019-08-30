b3d353c154021cb6487350c0e13dee42e61d2f91
doublejay96 <tanjinjie96@gmail.com>
2019-08-30 22:37:49 +0800

    :)

--- firebasePush.py
+++ firebasePush.py
@@ -14,14 +14,56 @@ from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create ADC object using I2C bus
-ads1 = ADS.ADS1115(i2c, address=0x49)
-ads2 = ADS.ADS1115(i2c, address=0x48)
+ads1 = ADS.ADS1115(i2c, address=0x48)
+ads2 = ADS.ADS1115(i2c, address=0x49)
 ads3 = ADS.ADS1115(i2c, address=0x4a)
+machineDict = {
+    1 : (ads1, ADS.P0),
+    2 : (ads1, ADS.P1),
+    3 : (ads1, ADS.P2),
+    4 : (ads1, ADS.P3),
+    5 : (ads2, ADS.P0),
+    6 : (ads2, ADS.P1),
+    7 : (ads2, ADS.P2),
+    8 : (ads2, ADS.P3),
+    9 : (ads3, ADS.P0)
+}
 
-# create single-ended input on channel 0
-#chan = [AnalogIn(ads, ADS.P0), AnalogIn(ads, ADS.P1)]
-#chan = [AnalogIn(ads, ADS.P1), AnalogIn(ads, ADS.P0)]
-#print("{:>5}\t{:>5}".format('raw', 'v'))
+machineConnectedDict = {
+    1 : True,
+    2 : True,
+    3 : False,
+    4 : False,
+    5 : True,
+    6 : True,
+    7 : False,
+    8 : False,
+    9 : True,
+}
+
+machineStatusDict = {
+    1 : "off",
+    2 : "off",
+    3 : "off",
+    4 : "off",
+    5 : "off",
+    6 : "off",
+    7 : "off",
+    8 : "off",
+    9 : "off",
+}
+
+machineStartTimeDict = {
+    1 : 0,
+    2 : 0,
+    3 : 0,
+    4 : 0,
+    5 : 0,
+    6 : 0,
+    7 : 0,
+    8 : 0,
+    9 : 0,
+}
 
 cred = credentials.Certificate('./usc-laundry-test-firebase-adminsdk-0sph5-01054b85e5.json')
 
@@ -36,31 +78,28 @@ machine0starttime = 0
 machine0status = "off"
 
 while True:
-    lightnessValue0 = AnalogIn(ads1, ADS.P0).value
-    #lightnessValue1 = AnalogIn(ads, ADS.P1).value
-    if lightnessValue0 < generalThresholdValue:
-        if machine0status == "off":
-            machine0starttime = time.time()
-        machine0status = "on"
-    else:
-        if machine0status == "on":
-            machine0time = 0
-        machine0status = "off"
-    #if lightnessValue1 > generalThresholdValue:
-    #    machine1status = "on"
-    #else:
-    #    machine1status = "off"
-    print("pin 0 " + str(lightnessValue0))
-    #print("pin 1 " + str(lightnessValue1))
-    if machine0status == "on":
-        machine0TimeRemaining = time.time() - machine0starttime
-    else:
-        machine0TimeRemaining = 0
-    doc_ref.set({
-        "machine0":machine0status,
-        "machine0TimeRemaining":machine0TimeRemaining
-    })
-    time.sleep(0.1)
+    for machineNumber in machineDict:
+        if machineConnectedDict[machineNumber] == False:
+            continue
+        lightnessValue = AnalogIn(machineDict[machineNumber][0], machineDict[machineNumber][1]).value
+        if lightnessValue < generalThresholdValue:
+            if machineStatusDict[machineNumber] == "off":
+                machineStartTimeDict[machineNumber] = time.time()
+            machineStatusDict[machineNumber] = "on"
+        else:
+            if machineStatusDict[machineNumber] == "on":
+                machineStartTimeDict[machineNumber] = 0
+            machineStatusDict[machineNumber] = "off"
+        print("pin value for machine " + str(machineNumber) + " is " + str(lightnessValue) + ", status is " + str(machineStatusDict[machineNumber]))
+        if machineStatusDict[machineNumber] == "on":
+            machineTimeRemaining = time.time() - machineStartTimeDict[machineNumber]
+        else:
+            machineTimeRemaining = 0
+        doc_ref.set({
+            "machine" + str(machineNumber):machineStatusDict[machineNumber],
+            "machine" + str(machineNumber) + "TimeRemaining":machineTimeRemaining
+        },merge=True)
+        time.sleep(0.1)
 
 #LDR resistance decreases with increasing light intensity
 #light on > lower ADC values
